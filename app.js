a79027ace938f4afc9f80e2cc9a5585b604cffad
Daniel Wangai <mainad57@gmail.com>
2019-01-02 18:25:25 +0300

    application setup - add express, mongoose and dotnet packages - setup express - add mongodb schema for user - connect to db

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+const DB_NAME = process.env.DB_NAME
+
+const DB_URL = "mongodb://"+DB_USER+":"+DB_PASSWORD+"@"+DB_HOST+":"+DB_PORT+"/"+DB_NAME
+
+// mongodb connection
+mongoose.connect(DB_URL)
+mongoose.connection.once("open", function() {
+    console.log("Connected to database...")
+})
+
+app.listen(4000, function() {
+    console.log("Listening on port 4000")
+})
---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: String,
+    username: String,
+    bio: String,
+    profilePic: String,
+    isAuthor: Boolean
+})
+
+module.exports = mongoose.model("User", UserSchema)
